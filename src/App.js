240692290de55e167a9caf2117a21fa51776772b
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-12-30 18:27:49 -0600

    repo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-tsparticles";
+import logo from './img/nave.png';
+import './scss/index.scss';
 
 function App() {
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <div className="App">
+      <section className="background-a">
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+
+            fpsLimit: 60,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 70,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 0,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              groups: {
+                z5000: {
+                  number: {
+                    value: 70,
+                  },
+                  zIndex: {
+                    value: 5000,
+                  },
+                },
+                z7500: {
+                  number: {
+                    value: 30,
+                  },
+                  zIndex: {
+                    value: 7500,
+                  },
+                },
+                z2500: {
+                  number: {
+                    value: 50,
+                  },
+                  zIndex: {
+                    value: 2500,
+                  },
+                },
+                z1000: {
+                  number: {
+                    value: 40,
+                  },
+                  zIndex: {
+                    value: 1000,
+                  },
+                },
+              },
+
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                enable: false,
+              },
+              collisions: {
+                enable: false,
+              },
+              move: {
+                angle: {
+                  value: 10,
+                  offset: 10,
+                },
+                enable: true,
+                speed: 0.3,
+                direction: "left",
+                random: true,
+                straight: true,
+                outModes: {
+                  default: "out",
+                },
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 21000,
+                },
+              },
+              number: {
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 0.3,
+              },
+
+            },
+            detectRetina: true,
+          }}
+        />
+      </section>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo go-pharma" />
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://go-pharma.mx/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go Pharma
         </a>
+        <p>
+
+          LA AGENCIA QUE VA CONTIGO
+
+        </p>
       </header>
     </div>
   );
