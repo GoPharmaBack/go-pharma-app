7e241a0c8f33e8d8366c990570d1f79c1b385165
Carlos Boyzo Ubuntu <carlos.boor@gmail.com>
2022-05-09 02:23:11 -0500

    ajuste de pantalla

--- src/views/Nosotros.js
+++ src/views/Nosotros.js
@@ -2,44 +2,91 @@ import React from 'react'
 import 'aos/dist/aos.css' // You can also use <link> for styles
 import Socios from '../views/Socios'
 import Team from '../components/Team'
+// const Star = 'https://firebasestorage.googleapis.com/v0/b/go-pharma-website.appspot.com/o/3d-elements%2FStar.png?alt=media&token=530337ee-8115-4965-86d0-4d1a713f4e3e'
+const Clay = 'https://firebasestorage.googleapis.com/v0/b/go-pharma-website.appspot.com/o/3d-elements%2Fclay%402x.png?alt=media&token=847a0e52-0aae-4730-b2fc-f58f896961e1'
 const Star = 'https://firebasestorage.googleapis.com/v0/b/go-pharma-website.appspot.com/o/3d-elements%2FStar.png?alt=media&token=530337ee-8115-4965-86d0-4d1a713f4e3e'
 
 function Nosotros () {
   return (
     <>
-      <section data-aos="fade-up" className="section nosotros">
-        <div className="contenedor">
-          <h1 data-aos="fade-up"> Nosotros</h1>
-
-          <h2>
-            Somos una agencia de <span>CONSULTORÍA, MKT &amp; MEDIA </span>
-            especializada en el mercado <span>PHARMA</span> y el sector
-            <span>SALUD. </span> Desarrollamos estrategias integradas desde
-            market research hasta la ejecución de la planeación, siempre
-            enfocados a <span>RESULTADOS.</span>
-          </h2>
-
-          <h3 data-aos="fade-up">Misión</h3>
-          <p data-aos="fade-up" className="h2">
-            Desarrollamos estrategias personalizadas encaminadas a que nuestros
-            clientes logren comunicar estratégicamente el valor de su producto o
-            servicio para alcanzar sus metas y objetivos{' '}
-          </p>
-          <h3 data-aos="fade-up">Nuestra Experiencia </h3>
-          <p data-aos="fade-up" className="h2">
-            Contamos con experiencia en manejo de productos de diversas áreas
-            terapéuticas:{' '}
-            <span>
-              ENFERMEDADES RARAS, NEUROLOGÍA, ONCOLOGÍA, REUMATOLOGÍA,
-              DERMATOLOGÍA, DISPOSITIVOS MÉDICOS, PRIMARY CARE,
-            </span>{' '}
-            entre otras.{' '}
-          </p>
+      <section className="section nosotros" data-aos="fade-up">
+        <div className="container">
+          <div className="row" data-aos="fade-up">
+            <div className="col">
+              <h1 className="title">Nosotros</h1>
+            </div>
+            <div className="col ">
+              <h2>Somos una agencia de <span>CONSULTORÍA, MKT &amp; MEDIA </span>especializada en el mercado <span>PHARMA</span> y el sector <span>SALUD. </span> Desarrollamos estrategias integradas desde market research hasta la ejecución de la planeación, siempre enfocados a <span>RESULTADOS.</span></h2>
+            </div>
+          </div>
+          <div className="contenedor container-3d" data-aos="fade-up" >
+            <img src={Clay} alt="nube" />
+          </div>
+          <div className="row">
+            <div className="contenedor-beneficios">
+              <div className="contenedor-beneficios-lista" data-aos="fade-up">
+                <div className="col">
+                  <img
+                    className="img-responsive"
+                    src={Star}
+                    alt="Go-pharma, MKT MEDIA especializada en el mercado PHARMA y el sector SALUD."
+                  />
+                  <p className="titulo" data-aos="fade-up">Misión</p>
+                  <p data-aos="fade-up" className="h4 aos-init aos-animate">Desarrollamos estrategias personalizadas encaminadas a que nuestros clientes logren comunicar estratégicamente el valor de su producto o servicio para alcanzar sus metas y objetivos </p>
+                </div>
+                <div className="col">
+                  <img
+                    className="img-responsive"
+                    src={Star}
+                    alt="Go-pharma, MKT MEDIA especializada en el mercado PHARMA y el sector SALUD."
+                  />
+                  <p className="titulo" data-aos="fade-up">Nuestra Experiencia</p>
+                  <p data-aos="fade-up" className="h4 aos-init aos-animate">Contamos con experiencia en manejo de productos de diversas áreas terapéuticas: <span>ENFERMEDADES RARAS, NEUROLOGÍA, ONCOLOGÍA, REUMATOLOGÍA, DERMATOLOGÍA, DISPOSITIVOS MÉDICOS, PRIMARY CARE,</span> entre otras. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="contenedor-beneficios pilares">
+              <h3 data-aos="fade-up" className='text-left'>NUESTROS PILARES</h3>
+              <div className="contenedor-beneficios-lista" data-aos="fade-up">
+                <div className="col p-2">
+                  <img
+                    className="img-responsive"
+                    src={Star}
+                    alt="Go-pharma, MKT MEDIA especializada en el mercado PHARMA y el sector SALUD."
+                  />
+                  <p className="titulo" data-aos="fade-up">Confianza</p>
+                  <p data-aos="fade-up" className="h4 aos-init aos-animate">Creamos vínculos basados en la credibilidad, co nfianza compromiso y vulnerabilidad. </p>
+                </div>
+                <div className="col p-2">
+                  <img
+                    className="img-responsive"
+                    src={Star}
+                    alt="Go-pharma, MKT MEDIA especializada en el mercado PHARMA y el sector SALUD."
+                  />
+                  <p className="titulo" data-aos="fade-up">Organización</p>
+                  <p data-aos="fade-up" className="h4 aos-init aos-animate">Trabajamos en equipo cuidando al máximo cada detalle.
+                  </p>
+                </div>
+                <div className="col p-2">
+                  <img
+                    className="img-responsive"
+                    src={Star}
+                    alt="Go-pharma, MKT MEDIA especializada en el mercado PHARMA y el sector SALUD."
+                  />
+                  <p className="titulo" data-aos="fade-up">Constancia</p>
+                  <p data-aos="fade-up" className="h4 aos-init aos-animate">Mantenemos el orden y constancia en todo momento.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
 
       <section className="section filosofia" data-aos="fade-up">
-        <div className="contenedor ">
+        <div className="container-fluid ">
           <div className="texto">
             <h3 data-aos="fade-up">NUESTRA FILOSOFÍA</h3>
             <h4 data-aos="fade-up"> PATIENT CENTRICITY </h4>
@@ -49,33 +96,6 @@ function Nosotros () {
             <h4 data-aos="fade-up">CUSTOMER ENGAGEMENT </h4>
           </div>
         </div>
-      </section>
-      <section data-aos="fade-up">
-        <div className="contenedor pilares" data-aos="fade-up">
-          <h1 data-aos="fade-up" className="aos-init aos-animate">PILARES</h1>
-          <div className="contenedor-pilares">
-            <div data-aos="fade-up" className="col">
-              <img src={Star} className="img-star " alt="" />
-              <p className="h2">
-                Creamos vínculos basados en la credibilidad, confianza compromiso
-                y vulnerabilidad.
-              </p>
-            </div>
-            <div data-aos="fade-up" className="col">
-              <img src={Star} className="img-star " alt="" />
-              <p className="h2">
-                Trabajamos en equipo cuidando al máximo cada detalle.{' '}
-              </p>
-            </div>
-            <div data-aos="fade-up" className="col">
-              <img src={Star} className="img-star " alt="" />
-              <p className="h2">
-                Mantenemos el orden y constancia en todo momento.{' '}
-              </p>
-            </div>
-          </div>
-        </div>
-
       </section>
 
       <Team />
